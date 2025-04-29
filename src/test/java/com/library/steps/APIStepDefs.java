717c4cb884b6fd399b6b823706eee1d83907bfce
Illeria <sireilleria@gmail.com>
2025-04-29 18:30:46 -0400

    Added User Story 1

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -1,6 +1,7 @@
 package com.library.steps;
 
 import com.library.utility.LibraryAPI_Util;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,15 +11,54 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
-import org.junit.Assert;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.hamcrest.Matchers.*;
 
 public class APIStepDefs {
+
+    /*---------- US01 ----------*/
+    //Global variables:
+    RequestSpecification givenPart = RestAssured.given().log().uri();
+    Response response;
+    ValidatableResponse thenPart;
+    JsonPath jp;
+
+    String expectedID;
+
+    @Given("I logged Library api as a {string}")
+    public void i_logged_library_api_as_a(String role){
+        givenPart.header("x-library-token", LibraryAPI_Util.getToken(role));
+
+        givenPart.log().all();
+    }
+
+    @Given("Accept header is {string}")
+    public void accept_header_is(String acceptHeader) {
+        givenPart.accept(acceptHeader);
+    }
+
+    @When("I send GET request to {string} endpoint")
+    public void i_send_get_request_to_endpoint(String endpoint) {
+        response = givenPart.when().get(endpoint);
+        jp = response.jsonPath();
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(int expectedStatusCode) {
+        thenPart = response.then();
+        thenPart.statusCode(expectedStatusCode);
+    }
+
+    @Then("Response Content type is {string}")
+    public void response_content_type_is(String expectedContentType) {
+        thenPart.contentType(expectedContentType);
+    }
+
+    @Then("Each {string} field should not be null")
+    public void each_field_should_note_be_null(String path) {
+        thenPart.body(path, Matchers.everyItem(Matchers.notNullValue()));
+    }
+
+
+
 
 
 }
